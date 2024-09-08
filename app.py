8c63ba7dd502b3b0896fa7a10037f20085ac3ba2
mogbojuri segun <ademegcollege@gmail.com>
2024-09-08 12:35:37 +0100

    Updated Flask app to bind to the correct port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -37,5 +38,5 @@ def index():
     return render_template('index.html', weather_info=weather_info)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
+    port = int(os.environ.get('PORT', 8080))  # Use PORT from environment or default to 8080
+    app.run(host='0.0.0.0', port=port, debug=True)
